f7956d4ff302a160c6d56e636c3e0dd8f6f50b00
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-08-08 20:16:50 +0930

    main-2-5.cpp

---
+++ main-2-5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+extern bool is_descending(int[], int);
+
+int main() {
+    int array1[5] = {1,2,3,4,5}; //test out 5 ascending positive integers
+    std::cout << "1 \n";
+    is_descending(array1, 5);
+
+    int array2[4] = {-4,-5,-6,-9}; //test out 4 negative descending integers
+    std::cout << "2 \n";
+    is_descending(array2, 4);
+
+    int array3[3] = {-50, 160, 40}; //test out larger, mixed signs not ascending or descending
+    std::cout << "3 \n";
+    is_descending(array3, 3);
+
+    int array4[3] = {45,-50,160}; //test out output if the size perameter is less than 1
+    std::cout << "4 \n";
+    is_descending(array4, 0);
+
+    int array5[5] = {5,4,3,2,1}; //test out 5 descending positive integers
+    std::cout << "5 \n";
+    is_descending(array5, 5);
+
+    return 0;
+}
