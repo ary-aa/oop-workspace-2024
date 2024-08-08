36a54b6a517ce9a14b62eff00619c27adb6ec038
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-08-08 20:10:21 +0930

    function-2-3.cpp

---
+++ function-2-3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+// returns the number of 2's, 5's and 9's in a given array
+void two_five_nine(int array[], int n)
+{
+    int num_twos = 0;
+    int num_fives = 0;
+    int num_nines = 0;
+
+    for ( int i = 0;  i < n; i++ ){
+        switch(array[i]){
+            case 2:
+                num_twos++;
+                break;
+            case 5:
+                num_fives++;
+                break;
+            case 9:
+                num_nines++;
+                break;
+        }
+
+    }
+    std::cout << "2:" <<num_twos<< ";5:" <<num_fives<< ";9:" <<num_nines<< ";\n";
+    return ;
+}
