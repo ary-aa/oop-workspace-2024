96629a13c1abe050dda959eeab3f2ae7c4395cbd
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-08-09 15:41:52 +0930

    main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,12 +1,15 @@
 // main-1-2.cpp
 #include <iostream>
+
+void modifyArray(double* array, int size, double value); 
+
 int main() {
     double myArray[] = {3.5, 2.1, 5.9, 1.4, 7.2}; 
-    int size = sizeof(myArray) / sizeof(myArray[0]); 
+    int size = sizeof(myArray) / sizeof(myArray[0]);
 
     double valueToAdd = 2.0; 
     modifyArray(myArray, size, valueToAdd); 
-
+    
     std::cout << "Modified array: ";
     for (int i = 0; i < size; ++i) {
         std::cout << myArray[i] << " ";
