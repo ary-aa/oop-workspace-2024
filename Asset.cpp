1692b8d891d6a7f5d9a76641458f8cfb00890e7b
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-08-23 16:04:28 +0930

    Update Asset.cpp

--- Asset.cpp
+++ Asset.cpp
@@ -1,20 +1,14 @@
 #include "Asset.h"
 
-// Default constructor
 Asset::Asset() : value(0), product_type("") {}
-
-// Constructor that takes the product type and value
 Asset::Asset(int value, std::string product_type) : value(value), product_type(product_type) {}
 
-// Getter for product type
 std::string Asset::get_product_type() {
     return product_type;
 }
 
-// Getter for value
 int Asset::get_value() {
     return value;
 }
 
-// Destructor
 Asset::~Asset() {}
