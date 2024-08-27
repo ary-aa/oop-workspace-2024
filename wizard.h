c2fbbf3d379046691dbf203937d3f519cfe00f24
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-08-27 19:39:00 +0930

    wizard.h

---
+++ wizard.h
@@ -0,0 +1,17 @@
+#ifndef WIZARD_H
+#define WIZARD_H
+
+#include "player.h"
+
+class Wizard : public Player {
+private:
+    int mana;
+
+public:
+    Wizard(std::string name, int health, int damage, int mana);
+    void castSpell(Player* opponent);
+    int getMana() const;
+    void setMana(int mana);
+};
+
+#endif // WIZARD_H
