1f18e37ad6e5381c2b193f13e9bf7a40dac0bec2
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-09-10 17:47:47 +0930

    AirVehicle.h

--- AirVehicle.h
+++ AirVehicle.h
@@ -6,6 +6,27 @@ class AirVehicle {
 public:
     AirVehicle();
     AirVehicle(int w);
-    AirVehicle (float f);
-    AirVehicle (int flights);
-}
+
+    // Member functions
+    void refuel();
+    virtual void fly(int headwind, int minutes);
+
+    // Getters
+    int get_weight() const;
+    float get_fuel() const;
+    int get_numberOfFlights() const;
+
+    // Setters
+    void set_weight(int w);
+    void set_fuel(float f);
+    void set_numberOfFlights(int flights);
+
+private:
+    int weight;
+    float fuel;
+    int numberOfFlights;
+};
+
+#endif // AIRVEHICLE_H
+
+
