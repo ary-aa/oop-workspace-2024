282ab96313cf7a4b0711b7857a87bfeb881c04d6
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-09-10 18:16:25 +0930

    Update Airfleet.h

--- Airfleet.h
+++ Airfleet.h
@@ -1,1 +1,20 @@
+// AirFleet.h
+#ifndef AIRFLEET_H
+#define AIRFLEET_H
 
+#include "AirVehicle.h"
+#include "Airplane.h"
+#include "Helicopter.h"
+
+class AirFleet {
+public:
+    AirFleet();
+    ~AirFleet(); // Destructor
+
+    AirVehicle** get_fleet() const;
+
+private:
+    AirVehicle** fleet;
+};
+
+#endif // AIRFLEET_H
