729b6bc719c9158c2da22599aac913f72066fac6
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-10-25 16:44:16 +1030

    Update Interactable.hpp

--- Interactable.hpp
+++ Interactable.hpp
@@ -1,42 +1,49 @@
 #ifndef INTERACTABLE_HPP
 #define INTERACTABLE_HPP
 
-#include "Explorer.hpp"
 #include "GridItem.hpp"
 
-enum InteractableType { EXIT, PIT };
+class Explorer; // Forward declaration
+
+// Enumeration for Interactable types
+enum InteractableType {
+    EXIT,
+    PIT
+};
 
 class Interactable : public GridItem {
 private:
-    static int activeInteractableCount;
+    static int activeCount; // Static count of active Interactable objects
 
 public:
-    // Constructor
-    Interactable(int x, int y, int width, int height) : GridItem(x, y, width, height) {
-        activeInteractableCount++;
-    }
-
     // Default constructor
-    Interactable() : GridItem() {
-        activeInteractableCount++;
+    Interactable() : GridItem(0, 0, 0, 0) {
+        activeCount++;
     }
 
-    // Virtual destructor
-    virtual ~Interactable() {
-        activeInteractableCount--;
+    // Parameterized constructor
+    Interactable(int x, int y, int width, int height) : GridItem(x, y, width, height) {
+        activeCount++;
     }
 
-    // Static function to get count of active Interactable objects
-    static int getActiveInteractableCount() {
-        return activeInteractableCount;
-    }
-
-    // Pure virtual functions
+    // Pure virtual function for interaction
     virtual bool interact(Explorer* player) = 0;
+
+    // Pure virtual function to get the type
     virtual InteractableType getType() = 0;
+
+    // Get active Interactable count
+    static int getActiveInteractableCount() {
+        return activeCount;
+    }
+
+    // Destructor
+    virtual ~Interactable() {
+        activeCount--;
+    }
 };
 
-// Initialize static variable
-int Interactable::activeInteractableCount = 0;
+// Initialize static member
+int Interactable::activeCount = 0;
 
-#endif
+#endif // INTERACTABLE_HPP
