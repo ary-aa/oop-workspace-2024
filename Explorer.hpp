e29c73b731283b727c13e05623f0e3e8fefb089b
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-10-25 16:44:41 +1030

    Update Explorer.hpp

--- Explorer.hpp
+++ Explorer.hpp
@@ -1,20 +1,43 @@
+#ifndef EXPLORER_HPP
+#define EXPLORER_HPP
+
 #include "GridItem.hpp"
-#include <iostream>
 
 class Explorer : public GridItem {
+private:
+    int stamina; // Stamina of the Explorer
+
 public:
-    Explorer(int x, int y, int width, int height) : GridItem(x, y, width, height) {}
+    // Constructor
+    Explorer(int gridWidth, int gridHeight) : GridItem(0, 0, gridWidth, gridHeight), stamina(2) {}
 
+    // Get current stamina
+    int getStamina() {
+        return stamina;
+    }
+
+    // Jumping a pit
+    void jumpPit() {
+        if (stamina > 0) {
+            stamina--;
+        }
+    }
+
+    // Move function
     bool move(int xOffset, int yOffset) {
-        int newX = getX() + xOffset;
-        int newY = getY() + yOffset;
+        // Check if the move is legal (only one direction)
+        if ((xOffset != 0 && yOffset == 0) || (xOffset == 0 && yOffset != 0)) {
+            int newX = std::get<0>(getCoordinates()) + xOffset;
+            int newY = std::get<1>(getCoordinates()) + yOffset;
 
-        if (newX >= 0 && newY >= 0) {
-            setX(newX);
-            setY(newY);
-            std::cout << "new position: " << getX() << ", " << getY() << std::endl;
-            return true;
+            // Check bounds
+            if (newX >= 0 && newX < getGridWidth() && newY >= 0 && newY < getGridHeight()) {
+                setCoordinates(newX, newY);
+                return true;
+            }
         }
         return false;
     }
 };
+
+#endif // EXPLORER_HPP
