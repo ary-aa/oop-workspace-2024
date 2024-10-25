785c086715ba5b403bf92ce5f333faf423b0e578
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-10-25 16:27:28 +1030

    Update Helper.hpp

--- Helper.hpp
+++ Helper.hpp
@@ -6,14 +6,17 @@
 
 class Helper {
 public:
-    // Calculate absolute squared distance between two points
+    // Calculate squared Euclidean distance between two points
     static double absoluteSquaredDistance(std::tuple<int, int> item1, std::tuple<int, int> item2) {
         int x1 = std::get<0>(item1);
         int y1 = std::get<1>(item1);
         int x2 = std::get<0>(item2);
         int y2 = std::get<1>(item2);
-
-        return std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2);
+        
+        // Corrected squared distance formula
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        return (dx * dx) + (dy * dy);
     }
 };
 
