2c93c6fe3cb519e524feb2fb0d7f409d27b6b8b3
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-10-25 16:52:20 +1030

    Update Exit.hpp

--- Exit.hpp
+++ Exit.hpp
@@ -2,18 +2,16 @@
 #define EXIT_HPP
 
 #include "Interactable.hpp"
+#include "Helper.hpp"
 
 class Exit : public Interactable {
 public:
-    // Constructor
     Exit(int width, int height) : Interactable(width - 1, height - 1, 1, 1) {}
 
-    // Interact function
     bool interact(Explorer* player) override {
         return Helper::absoluteSquaredDistance(getCoordinates(), player->getCoordinates()) == 0;
     }
 
-    // Get type function
     InteractableType getType() override {
         return EXIT;
     }
