d3bd965f7414c25b928f3d1edd79a0f7602bea84
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-10-25 16:37:04 +1030

    Update GridItem.hpp

--- GridItem.hpp
+++ GridItem.hpp
@@ -1,57 +1,20 @@
 #ifndef GRIDITEM_HPP
 #define GRIDITEM_HPP
 
-#include <tuple>
-
 class GridItem {
 private:
     int x, y, width, height;
-    static int activeGridItemCount;
 
 public:
-    // Default constructor
-    GridItem() : x(0), y(0), width(0), height(0) {
-        activeGridItemCount++;
-    }
+    GridItem(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {}
 
-    // Parameterized constructor
-    GridItem(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {
-        activeGridItemCount++;
-    }
+    // Getters for x and y
+    int getX() const { return x; }
+    int getY() const { return y; }
 
-    // Destructor
-    virtual ~GridItem() {
-        activeGridItemCount--;
-    }
-
-    // Set coordinates
-    void setCoordinates(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
-
-    // Get coordinates
-    std::tuple<int, int> getCoordinates() const {
-        return std::make_tuple(x, y);
-    }
-
-    // Get grid width
-    int getGridWidth() const {
-        return width;
-    }
-
-    // Get grid height
-    int getGridHeight() const {
-        return height;
-    }
-
-    // Get count of active GridItem objects
-    static int getActiveGridItemCount() {
-        return activeGridItemCount;
-    }
+    // Setters for x and y
+    void setX(int newX) { x = newX; }
+    void setY(int newY) { y = newY; }
 };
-
-// Initialize static variable
-int GridItem::activeGridItemCount = 0;
 
 #endif
