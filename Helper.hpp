4009856d6294f31b9894dcd38e628679c744eaed
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-10-25 16:42:22 +1030

    Update Helper.hpp

--- Helper.hpp
+++ Helper.hpp
@@ -2,21 +2,18 @@
 #define HELPER_HPP
 
 #include <tuple>
+#include <cmath>
 
 class Helper {
 public:
-    // Calculate squared Euclidean distance (returns an integer)
-    static int absoluteSquaredDistance(std::tuple<int, int> item1, std::tuple<int, int> item2) {
+    // Function to calculate the Euclidean squared distance between two coordinates
+    static double absoluteSquaredDistance(std::tuple<int, int> item1, std::tuple<int, int> item2) {
         int x1 = std::get<0>(item1);
         int y1 = std::get<1>(item1);
         int x2 = std::get<0>(item2);
         int y2 = std::get<1>(item2);
-        
-        // Calculate squared Euclidean distance
-        int dx = x2 - x1;
-        int dy = y2 - y1;
-        return (dx * dx) + (dy * dy);
+        return std::pow(std::abs(x2 - x1), 2) + std::pow(std::abs(y2 - y1), 2);
     }
 };
 
-#endif
+#endif // HELPER_HPP
