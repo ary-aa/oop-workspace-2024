fb6c4a70d4512cd3f78e0f974b8ed74c9b8b5e83
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-10-25 16:42:38 +1030

    Update main-1.cpp

--- main-1.cpp
+++ main-1.cpp
@@ -3,30 +3,26 @@
 #include "Helper.hpp"
 
 int main() {
-    // Test GridItem default constructor
-    GridItem item1;
-    std::cout << "Default GridItem coordinates: (" 
-              << std::get<0>(item1.getCoordinates()) << ", " 
-              << std::get<1>(item1.getCoordinates()) << ")\n";
-
-    // Test GridItem parameterized constructor
-    GridItem item2(5, 7, 10, 10);
-    std::cout << "Parameterized GridItem coordinates: (" 
-              << std::get<0>(item2.getCoordinates()) << ", " 
-              << std::get<1>(item2.getCoordinates()) << ")\n";
+    // Testing GridItem class
+    GridItem item1; // Default constructor
+    std::cout << "Item1 coordinates: (" << std::get<0>(item1.getCoordinates())
+              << ", " << std::get<1>(item1.getCoordinates()) << ")\n";
     
-    // Test active count
-    std::cout << "Active GridItem objects: " << GridItem::getActiveGridItemCount() << "\n";
+    GridItem item2(3, 4, 1, 1); // Parameterized constructor
+    std::cout << "Item2 coordinates: (" << std::get<0>(item2.getCoordinates())
+              << ", " << std::get<1>(item2.getCoordinates()) << ")\n";
+    
+    // Testing setCoordinates
+    item1.setCoordinates(1, 2);
+    std::cout << "Item1 new coordinates: (" << std::get<0>(item1.getCoordinates())
+              << ", " << std::get<1>(item1.getCoordinates()) << ")\n";
 
-    // Test setting new coordinates
-    item2.setCoordinates(2, 3);
-    std::cout << "New coordinates of item2: (" 
-              << std::get<0>(item2.getCoordinates()) << ", " 
-              << std::get<1>(item2.getCoordinates()) << ")\n";
+    // Testing active item count
+    std::cout << "Active GridItem count: " << GridItem::getActiveGridItemCount() << "\n";
 
-    // Test Euclidean distance
-    double dist = Helper::absoluteSquaredDistance(item1.getCoordinates(), item2.getCoordinates());
-    std::cout << "Squared distance between item1 and item2: " << dist << "\n";
+    // Testing Helper class
+    double distance = Helper::absoluteSquaredDistance(item1.getCoordinates(), item2.getCoordinates());
+    std::cout << "Absolute squared distance between Item1 and Item2: " << distance << "\n";
 
     return 0;
 }
