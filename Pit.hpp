afe17d216670ea4b78e8144da45283c64941f4a1
ary-aa <126465871+ary-aa@users.noreply.github.com>
2024-10-25 16:45:08 +1030

    Update Pit.hpp

--- Pit.hpp
+++ Pit.hpp
@@ -1,34 +1,30 @@
 #ifndef PIT_HPP
 #define PIT_HPP
 
-#include "Explorer.hpp"
 #include "Interactable.hpp"
-#include "Helper.hpp"
 
 class Pit : public Interactable {
 private:
-    bool covered;
+    bool covered; // State of the pit (covered or not)
 
 public:
     // Constructor
     Pit(int x, int y, int width, int height) : Interactable(x, y, width, height), covered(false) {}
 
-    // Override interact function
+    // Interact function
     bool interact(Explorer* player) override {
-        if (Helper::absoluteSquaredDistance(player->getCoordinates(), getCoordinates()) == 0 && !covered) {
+        if (Helper::absoluteSquaredDistance(getCoordinates(), player->getCoordinates()) == 0 && !covered) {
             player->jumpPit();
             covered = true;
-            if (player->getStamina() == 0) {
-                return true; // Explorer loses if stamina is 0
-            }
+            return player->getStamina() == 0; // Return true if stamina is 0 after jumping
         }
         return false;
     }
 
-    // Override getType function
+    // Get type function
     InteractableType getType() override {
         return PIT;
     }
 };
 
-#endif
+#endif // PIT_HPP
